b4481a178fd0383dee57d52e5cbb67ca1d9106ce
afeiship <1290657123@qq.com>
2021-01-24 18:02:55 +0800

    feat: stringify/parse done

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,10 +1,12 @@
 (function () {
   const NxSlateSerialize = require('../src');
+  const { jsx } = require('slate-hyperscript');
 
   describe('NxSlateSerialize.methods', function () {
     test('serialize is text node', function () {
       var node = { text: 'a node' };
-      expect(NxSlateSerialize.serialize(node)).toBe('a node');
+      var process = (node) => node.text;
+      expect(NxSlateSerialize.serialize(node, { process })).toBe('a node');
     });
     test('nested text node for special process', () => {
       var node = {
@@ -51,6 +53,72 @@
       expect(NxSlateSerialize.serialize(node, options)).toBe(
         `<p>An opening paragraph with a <a href="https://example.com">link</a> in it.</p><blockquote><p>A wise quote.</p></blockquote><p>A closing paragraph!</p>`
       );
+    });
+
+    test.only('slate parse(deserialize) html to nodes', () => {
+      var process = (el, children) => {
+        switch (el.nodeName) {
+          case 'BODY':
+            return jsx('fragment', {}, children);
+          case 'BR':
+            return '\n';
+          case 'BLOCKQUOTE':
+            return jsx('element', { type: 'quote' }, children);
+          case 'P':
+            return jsx('element', { type: 'paragraph' }, children);
+          case 'A':
+            return jsx('element', { type: 'link', url: el.getAttribute('href') }, children);
+          default:
+            return el.textContent;
+        }
+      };
+
+      var html = `<p>An opening paragraph with a <a href="https://example.com">link</a> in it.</p><blockquote><p>A wise quote.</p></blockquote><p>A closing paragraph!</p>`;
+      var nodes = NxSlateSerialize.parse(html, { process });
+
+      expect(nodes).toEqual([
+        {
+          type: 'paragraph',
+          children: [
+            {
+              text: 'An opening paragraph with a '
+            },
+            {
+              type: 'link',
+              url: 'https://example.com',
+              children: [
+                {
+                  text: 'link'
+                }
+              ]
+            },
+            {
+              text: ' in it.'
+            }
+          ]
+        },
+        {
+          type: 'quote',
+          children: [
+            {
+              type: 'paragraph',
+              children: [
+                {
+                  text: 'A wise quote.'
+                }
+              ]
+            }
+          ]
+        },
+        {
+          type: 'paragraph',
+          children: [
+            {
+              text: 'A closing paragraph!'
+            }
+          ]
+        }
+      ]);
     });
   });
 })();
--- dist/index.js
+++ dist/index.js
@@ -3,27 +3,24 @@
  * description: Serializing/deserializing html/text for slate.
  * homepage: https://github.com/afeiship/next-slate-serialize
  * version: 1.0.0
- * date: 2021-01-24 17:26:09
+ * date: 2021-01-24 18:02:46
  * license: MIT
  */
 
 (function () {
   var global = typeof window !== 'undefined' ? window : this || Function('return this')();
   var nx = global.nx || require('@jswork/next');
-  var slate = require('slate');
-  var Text = slate.Text;
-  var DEFAULT_OPTIONS = {
-    process: function (node, children) {
-      return node.text;
-    },
-    joined: '\n'
+  var isPlainObject = nx.isPlainObject || require('@jswork/next-is-plain-object');
+  var DEFAULT_OPTIONS = { process: nx.stubValue, joined: '\n' };
+  var isText = function (value) {
+    return isPlainObject(value) && typeof value.text === 'string';
   };
 
   var NxSlateSerialize = nx.declare('nx.SlateSerialize', {
     statics: {
       serialize: function (inNode, inOptions) {
         var options = nx.mix(null, DEFAULT_OPTIONS, inOptions);
-        if (Text.isText(inNode)) return inNode.text;
+        if (isText(inNode)) return inNode.text;
 
         const children = inNode.children
           .map(function (n) {
@@ -33,15 +30,34 @@
 
         return options.process(inNode, children);
       },
+      deserialize: function (inElement, inOptions) {
+        var options = nx.mix(null, DEFAULT_OPTIONS, inOptions);
+        var el = inElement || document.body;
+        if (el.nodeType === 3) {
+          return el.textContent;
+        } else if (el.nodeType !== 1) {
+          return null;
+        }
+
+        console.log(el);
+
+        const children = nx.slice(el.childNodes).map(function (node) {
+          return this.deserialize(node, options);
+        }, this);
+        return options.process(el, children);
+      },
       stringify: function (inNodes, inOptions) {
         var options = nx.mix(null, DEFAULT_OPTIONS, inOptions);
         return inNodes
           .map(function (node) {
-            return this.serialize(node, options);
+            return this.serialize(node, inOptions);
           }, this)
           .join(options.joined);
       },
-      parse: function (inString) {}
+      parse: function (inString, inOptions) {
+        var document = new DOMParser().parseFromString(inString, 'text/html');
+        return this.deserialize(document.body, inOptions);
+      }
     }
   });
 
